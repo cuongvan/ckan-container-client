ad7dbe3883d75d951d08fe8f9661767fd75341b2
Cuong Van <cuongvtien@gmail.com>
2020-02-29 19:23:24 +0700

    update variable names

--- ckanapp_lib/__init__.py
+++ ckanapp_lib/__init__.py
@@ -2,20 +2,20 @@ import os, requests
 
 CKAN_HOST_ENV = 'CKAN_HOST'
 JSON_URL_ENV = 'JSON_INPUT_URL'
-BINARY_URL_ENV = 'BINARY_INPUT_URL'
+FILE_URL_ENV = 'BINARY_INPUT_URL'
 
 def get_app_params():
     ckan_host = os.getenv(CKAN_HOST_ENV)
-    json_input_url = os.getenv(JSON_URL_ENV)
-    binary_input_url = os.getenv(BINARY_URL_ENV, None)
+    json_url = os.getenv(JSON_URL_ENV)
+    file_url = os.getenv(FILE_URL_ENV, None)
 
     # TODO: raise exception & handle?
     ret = {
         'ckan_host': ckan_host,
-        'json': requests.get(json_input_url).json(),
+        'json': requests.get(json_url).json(),
     }
 
-    if binary_input_url:
-        ret['file'] = requests.get(binary_input_url).content
+    if file_url:
+        ret['file'] = requests.get(file_url).content
 
     return ret
--- ckanapp_lib/tests/test_ckanapp_lib.py
+++ ckanapp_lib/tests/test_ckanapp_lib.py
@@ -1,5 +1,5 @@
 from urllib.parse import urljoin
-from ckanapp_lib import get_app_params, CKAN_HOST_ENV, JSON_URL_ENV, BINARY_URL_ENV
+from ckanapp_lib import get_app_params, CKAN_HOST_ENV, JSON_URL_ENV, FILE_URL_ENV
 
 from mock import patch
 import mock, os, io, requests
@@ -10,15 +10,15 @@ import sure
 localhost = 'http://localhost'
 ckan_host = localhost
 json_url = '{}:6000/12345/json'.format(localhost)
-binary_url = '{}:6000/12345/binary'.format(localhost)
+file_url = '{}:6000/12345/binary'.format(localhost)
 
 JSON_ONLY = {
     CKAN_HOST_ENV: ckan_host,
     JSON_URL_ENV: json_url,
 }
-JSON_AND_BIN = {
+JSON_AND_FILE = {
     **JSON_ONLY,
-    BINARY_URL_ENV: binary_url,
+    FILE_URL_ENV: file_url,
 }
 
 @patch.dict(os.environ, JSON_ONLY)
@@ -30,11 +30,11 @@ def test_response_keys_json_only():
     params.should.have.key('ckan_host')
     params.should.have.key('json')
 
-@patch.dict(os.environ, JSON_AND_BIN)
+@patch.dict(os.environ, JSON_AND_FILE)
 def test_response_keys_json_and_binary():
     with Mocker() as m:
         m.get(json_url, json={})
-        m.get(binary_url)
+        m.get(file_url)
         params = get_app_params()
 
     params.should.have.key('ckan_host')
@@ -49,16 +49,17 @@ def test_json_response():
         m.get(json_url, json=json_params)
         params = get_app_params()
 
-    params['json'].should.equal(json_params)
+    params.should.have.key('json').being.equal(json_params)
 
-@patch.dict(os.environ, JSON_AND_BIN)
+@patch.dict(os.environ, JSON_AND_FILE)
 def test_binary_response():
-    
+    file_content = b'binary input'
+
     with Mocker() as m:
         m.get(json_url, json={})
-        m.get(binary_url, content=b'binary input')
+        m.get(file_url, content=file_content)
 
         params = get_app_params()
 
-    params['file'].should.equal(b'binary input')
+    params.should.have.key('file').being.equal(file_content)
 
